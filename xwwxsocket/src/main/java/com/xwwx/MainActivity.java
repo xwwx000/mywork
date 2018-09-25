35ebba5e17e51decfde2ceaeebd49853aa9d864e
xwwx <13452010@qq.com>
2018-09-25 10:22:06 +0800

    首页添加注解

--- xwwxsocket/src/main/java/com/xwwx/MainActivity.java
+++ xwwxsocket/src/main/java/com/xwwx/MainActivity.java
@@ -2,7 +2,14 @@ package com.xwwx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ * @功能 首页
+ *
+ * @作者 xwwx
+ *
+ * @创建日期 2018/9/25 上午 10:20
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
