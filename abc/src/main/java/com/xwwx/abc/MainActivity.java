31734c407f4ca99ff85fa496e2777d4b62729f5f
xwwx <13452010@qq.com>
2018-09-25 09:42:29 +0800

    测试

--- abc/src/main/java/com/xwwx/abc/MainActivity.java
+++ abc/src/main/java/com/xwwx/abc/MainActivity.java
@@ -2,7 +2,14 @@ package com.xwwx.abc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ * @功能 
+ * 
+ * @作者 Administrator
+ *
+ * @创建日期 2018/9/25 0025 上午 9:41
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
